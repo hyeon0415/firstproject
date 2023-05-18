32e309551292a95f9df57d386fe2fc2bbdd0ee5f
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-05-18 23:53:11 +0900

    2023. 05. 18
    redirect

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @AllArgsConstructor //생성자
 @NoArgsConstructor  // 디폴트 생성자
 @ToString
+@Getter
 public class Article {
 
     @Id // 대표값을 지정 Like a 주민번호
