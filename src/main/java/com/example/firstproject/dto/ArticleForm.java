6be89c3853d2b90c5d606611f020ad445d20f44d
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-05-24 23:34:02 +0900

    2023. 05. 24
    JSON Ctl 생성 (get, post)

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,9 +2,13 @@ package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor //생성자
+@NoArgsConstructor  // 디폴트 생성자
+@Getter
 @ToString
 public class ArticleForm {
 
